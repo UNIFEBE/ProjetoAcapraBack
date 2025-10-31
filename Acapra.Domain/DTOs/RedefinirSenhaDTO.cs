530e404862ac12bcc96e11adfbf68c399a302b48
LuisFelipeBarnabe <luisrbarnabe@gmail.com>
2025-10-30 21:13:34 -0300

    Ajustando novamente redefinir sena

---
+++ Acapra.Domain/DTOs/RedefinirSenhaDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acapra.Domain.DTOs
+{
+    public class RedefinirSenhaDTO
+    {
+        public string email {  get; set; }
+        public string novaSenha {  get; set; }
+    }
+}
